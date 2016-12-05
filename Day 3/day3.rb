f2b5b1d1aa546c0041c70a14d81ba068637a8f1c
Sebastian Osiński <sebastian@tooploox.com>
2016-12-05 21:06:39 +0100

    Add second part of day 3

--- Day 3/day3.rb
+++ Day 3/day3.rb
@@ -1,13 +1,23 @@
-
-input = File.read("day3_input")
-
-lines = input.split("\n").map { |line| line.split(" ").map { |e| e.to_i  } }
-
 def is_valid_triangle(sides)
   sides[0] + sides[1] > sides[2] &&
   sides[1] + sides[2] > sides[0] &&
   sides[0] + sides[2] > sides[1]
 end
+
+input = File.read("day3_input")
+
+#first part
+lines = input.split("\n").map { |line| line.split(" ").map { |e| e.to_i  } }
+
+#second part
+
+sides = lines.flatten
+
+aSides = sides.select.with_index { |e, i| [0, 1, 2].include?(i % 9) }
+bSides = sides.select.with_index { |e, i| [3, 4, 5].include?(i % 9) }
+cSides = sides.select.with_index { |e, i| [6, 7, 8].include?(i % 9) }
+
+lines = aSides.zip(bSides, cSides)
 
 validCount = lines.inject(0) { |sum, sides| sum + (is_valid_triangle(sides) ? 1 : 0) }
 
