95412e688e9ea9a6b502148b91443da37d325ad4
Sebastian Osiński <sebastian@tooploox.com>
2016-12-12 23:41:27 +0100

    Add solution for part 2 of Day 4

--- Day 4/day4.rb
+++ Day 4/day4.rb
@@ -1,9 +1,11 @@
 #input = "aaaaa-bbb-z-y-x-123[abxyz]\na-b-c-d-e-f-g-h-987[abcde]\nnot-a-real-room-404[oarel]\ntotally-real-room-200[decoy]"
 input = File.read("day4_input")
+
+#rooms = ["qzmt-zixmtkozy-ivhz-343[zimth]"]
 rooms = input.split("\n")
 
 def room_valid?(name, checksum)
-	name.gsub!("-", "")
+	name = name.gsub("-", "")
 	
 	letters_count = Hash.new(0)
 	
@@ -23,12 +25,41 @@ def room_valid?(name, checksum)
 	valid_checksum == checksum
 end
 
+def tokenize_room(room) 
+	room.match(/(\A[a-z\-]+)-(\d+)\[([a-z]+)\]\z/).captures
+end
+
 def sum_of_valid_room_ids(rooms)
 	rooms.reduce(0) do |acc, room|
-		name, id, checksum = room.match(/(\A[a-z\-]+)-(\d+)\[([a-z]+)\]\z/).captures
+		name, id, checksum = tokenize_room(room)
 		
 	 	room_valid?(name, checksum) ? acc + id.to_i : acc
 	end
 end
 
 puts sum_of_valid_room_ids(rooms)
+
+# second part
+
+def decode(name, id)
+	id = id.to_i
+	
+	a = "a".ord
+	z = "z".ord + 1
+	
+	l = z - a
+	
+	n = id % l
+	
+	name.chars.map { |c| c == '-' ? ' ' : (((c.ord + n - a) % l) + a).chr }.join("")
+end
+
+def decode_valid_room_names(rooms)
+	rooms.each do |room|
+		name, id, checksum = tokenize_room(room)
+		
+		puts "#{decode(name, id)} #{id}" if room_valid?(name, checksum)
+	end
+end
+
+decode_valid_room_names(rooms)
