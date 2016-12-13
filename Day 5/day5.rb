c60c0175dcaab174ed74fbaa779cb9a1db7f0d26
Sebastian Osiński <sebastian@tooploox.com>
2016-12-13 09:07:48 +0100

    Add solution for second part of Day 5

--- Day 5/day5.rb
+++ Day 5/day5.rb
@@ -8,13 +8,36 @@ index = 0
 password = ""
 
 while password.length < 8 do
-	digest = md5.hexdigest(door_id + index.to_s)
-	
-	if digest.start_with?("00000")
-		password += digest[5]
-	end
-	
-	index += 1
+  digest = md5.hexdigest(door_id + index.to_s)
+
+  if digest.start_with?("00000")
+    password += digest[5]
+  end
+
+  index += 1
 end
 
 puts password
+
+
+# second part
+
+password = Array.new(8)
+known_chars_count = 0
+index = 0
+
+while known_chars_count < 8 do
+  digest = md5.hexdigest(door_id + index.to_s)
+
+  if digest.start_with?("00000") && /\d/ === digest[5]
+  position = digest[5].to_i
+    if position < 8 && password[position] == nil
+      password[digest[5].to_i] = digest[6]
+      known_chars_count += 1
+    end
+  end
+
+  index += 1
+end
+
+puts password.join("")
