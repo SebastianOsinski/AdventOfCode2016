8c5a8ff96383b0a0185df0da9d00b36fd383a8df
Sebastian Osiński <sebastian@tooploox.com>
2016-12-13 11:49:38 +0100

    Add solution for part one of Day 6

---
+++ Day 6/day6.rb
@@ -0,0 +1,18 @@
+# input = "eedadn\ndrvtee\neandsr\nraavrd\natevrs\ntsrnev\nsdttsa\nrasrtv\nnssdts\nntnada\nsvetve\ntesnvt\nvntsnd\nvrdear\ndvrsen\nenarar"
+input = File.read('day6_input')
+
+lines = input.split("\n")
+
+n = lines[0].size
+
+columns = lines.each_with_object(Array.new(n) { [] }) do |line, acc|
+  (0...n).each do |i|
+    acc[i] << line[i]
+  end
+end
+password = columns.map do |column|
+  chars_count = column.each_with_object(Hash.new(0)) { |elem, acc| acc[elem] += 1 }
+  column.max_by { |v| chars_count[v] }
+end
+
+puts password.join('')
