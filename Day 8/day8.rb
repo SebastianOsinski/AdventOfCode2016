88e7541e26e63a26f09c74ea3ac1dab6c8d29e49
Sebastian Osiński <sebastian@tooploox.com>
2016-12-15 09:56:46 +0100

    Remove redundant print

--- Day 8/day8.rb
+++ Day 8/day8.rb
@@ -8,11 +8,11 @@ operations = parser.parse(input)
 screen = Screen.new(50, 6)
 
 operations.each do |operation|
-  puts operation
   first = operation.first_argument
   second = operation.second_argument
   screen.send(operation.type, first, second)
   puts screen
+  puts "\n"
 end
 
 puts screen.lit_pixels_count
